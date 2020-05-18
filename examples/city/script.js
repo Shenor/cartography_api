7173a5879246a9665f4041624ed499fb3bf83506
Shenor <dreik7570000@gmail.com>
2020-05-18 16:43:43 +0300

    Add files via upload

---
+++ examples/city/script.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    $('[name="city"]').fias({
+		type: $.fias.type.city,
+        change: function (obj) {
+            var address = $.fias.getAddress('.js-form-address');
+
+            $('#address').text(address);
+        },
+        'withParents' : true
+
+    });
+});
